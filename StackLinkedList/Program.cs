cc1676607b5592257f840a422476b39d846647c6
MehdiSaadati <108065298+MahdiSaadatiOrg@users.noreply.github.com>
2022-08-15 12:54:14 +0430

    Add files via upload

---
+++ StackLinkedList/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using StackLinkedList;
+
+Console.WriteLine("Hello, World!");
+
+
+
+Stack_LinkedList a = new Stack_LinkedList();
+
+
+a.Push(1);
+a.Pop();
+a.Pop();
+
+a.Push(2);
+a.Pop();
+
+a.Push(3);
+
+a.Print();
+
+a.Pop();
+a.Pop();
+a.Pop();
+a.Pop();
+
+a.Print();
---
+++ StackLinkedList/Stack_LinkedList.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackLinkedList
+{
+    class Node 
+    {
+        public int Value;
+        public Node Next;
+        public Node Prive;
+    }
+
+    public class Stack_LinkedList
+    {
+        Node head;
+        Node top;
+
+        bool IsEmpty()
+        {
+            if (top == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Push(int val)
+        {
+            if (IsEmpty())
+            {
+                Node newnode = new Node();
+                newnode.Value = val;
+                head = newnode;
+                top = head;
+                head.Next = null;
+                head.Prive = null;
+            }
+            else
+            {
+                Node newnode = new Node();
+                newnode.Value = val;
+
+                top.Next = newnode;
+                newnode.Prive = top;
+                top = newnode;
+            }
+        }
+
+        public void Pop() 
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Empty!!!");
+            }
+            else
+            {
+                Node temp = top.Prive;
+                top = null;
+                top = temp;
+            }
+        }
+
+        public void Print() 
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("there is nothing to print");
+            }
+            else
+            {
+                Node temp = head;
+                while (temp != null)
+                {
+                    Console.WriteLine(temp.Value);
+                    temp = temp.Next;
+                }
+            }
+        }
+    }
+}
